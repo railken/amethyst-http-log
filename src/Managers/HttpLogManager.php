4043cf093dc2bf501679bc61258b71aaee1ec398
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/HttpLogManager.php
+++ src/Managers/HttpLogManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\HttpLog newEntity()
+ * @method \Amethyst\Schemas\HttpLogSchema getSchema()
+ * @method \Amethyst\Repositories\HttpLogRepository getRepository()
+ * @method \Amethyst\Serializers\HttpLogSerializer getSerializer()
+ * @method \Amethyst\Validators\HttpLogValidator getValidator()
+ * @method \Amethyst\Authorizers\HttpLogAuthorizer getAuthorizer()
+ */
 class HttpLogManager extends Manager
 {
     use ConfigurableManager;
